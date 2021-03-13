f8a23ba88829ee17a58f46af569e859a51fa6cbb
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-03-13 16:21:23 +0100

    Rename convert_bds_individual() as convert_bds_target()

---
+++ R/convert_bds_target.R
@@ -0,0 +1,31 @@
+#' Convert json BDS data for single individual to target object
+#'
+#' This function takes data from a json source and saves it as a an object
+#' of class \code{target}.
+#' @param txt a JSON string, URL or file
+#' @param schema A JSON string, URL or file that selects the JSON validation
+#' schema.
+#' @param \dots Additional parameter passed down to \code{fromJSON(txt, ...)}.
+#' @return An object of class \code{target} or \code{NULL}
+#' @author Stef van Buuren 2021
+#' @seealso \code{\link[jsonlite]{fromJSON}}
+#' @examples
+#' fn <- system.file("extdata", "allegrosultum", "client3.json", package = "jamestest")
+#' q <- convert_bds_target(fn)
+#' @export
+convert_bds_target <- function(txt = NULL, schema = NULL, ...) {
+
+  if (is.null(txt)) return(NULL)
+
+  # Check. Tranform json errors (e.g. no file, invalid json) into a
+  # warning, and exit with empty target object.
+  checked <- tryCatch(
+    expr = verify(txt, schema = schema, ...),
+    error = function(cnd) {
+      stop(conditionMessage(cnd))
+    }
+  )
+
+  # and produce proper target data object
+  convert_checked_target(checked)
+}
