152a6a8acdb2d447975eb694f38e9050d4e7b2a5
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-04-02 12:49:42 +0200

    Add check to persondata()

--- R/persondata.R
+++ R/persondata.R
@@ -7,6 +7,9 @@
 #' persondata(read_bds(fn))
 #' @export
 persondata <- function(x) {
-  attr(x, "person") %>%
-    select(-all_of(c("dob")))
+  p <- attr(x, "person")
+  if (is.null(p)) {
+    stop("Found no person attribute.")
+  }
+  p %>% select(-all_of(c("dob")))
 }
