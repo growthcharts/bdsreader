55185e87486b6032a662ad1ba6abec58f1335392
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-04-14 20:15:47 +0200

    Adapt testfiles to account for changed schema default

--- tests/testthat/test-read_bds.R
+++ tests/testthat/test-read_bds.R
@@ -1,8 +1,11 @@
+schema <- "bds_schema_str.json"
+# schema <- "bds_schema.json"
+
 # test the empty object
 js1 <- '{"OrganisatieCode":0,"ClientGegevens":{}}'
 test_that("handles the empty individual object", {
   expect_message(
-    read_bds(js1, schema = "bds_schema_str.json"),
+    read_bds(js1, schema = schema),
     "should have required property 'Elementen'"
   )
 })
@@ -11,41 +14,41 @@ jtf <- system.file("extdata", "test", paste0("test", 1:25, ".json"), package = "
 
 test_that("test1.json (client3.json) passes read_bds()", {
   expect_equal(
-    class(read_bds(jtf[1], schema = "bds_schema_str.json")),
+    class(read_bds(jtf[1], schema = schema)),
     c("tbl_df", "tbl", "data.frame")
   )
 })
 
 test_that("test2.json (missing Referentie) PASSES", {
   expect_equal(
-    class(read_bds(jtf[2], schema = "bds_schema_str.json")),
+    class(read_bds(jtf[2], schema = schema)),
     c("tbl_df", "tbl", "data.frame")
   )
 })
 
 test_that("test3.json (missing OrganisatieCode) MESS", {
   expect_message(
-    read_bds(jtf[3], schema = "bds_schema_str.json"),
+    read_bds(jtf[3], schema = schema),
     "should have required property 'OrganisatieCode'"
   )
 })
 
 test_that("test4.json (wrong type) MESS", {
   expect_message(
-    read_bds(jtf[4], schema = "bds_schema_str.json"),
+    read_bds(jtf[4], schema = schema),
     ".OrganisatieCode should be integer"
   )
 })
 
 test_that("test5.json (missing ClientGegevens) MESS", {
   expect_message(
-    read_bds(jtf[5], schema = "bds_schema_str.json"),
+    read_bds(jtf[5], schema = schema),
     "should have required property 'ClientGegevens'"
   )
 })
 
 test_that("test6.json (Missing ContactMomenten) MESS", {
-  expect_message(read_bds(jtf[6], schema = "bds_schema_str.json"),
+  expect_message(read_bds(jtf[6], schema = schema),
     "Missing 'Contactmomenten'",
     fixed = TRUE
   )
@@ -53,108 +56,108 @@ test_that("test6.json (Missing ContactMomenten) MESS", {
 
 test_that("test7.json (Missing Referentie & OrganisatieCode) MESS", {
   expect_message(
-    read_bds(jtf[7], schema = "bds_schema_str.json"),
+    read_bds(jtf[7], schema = schema),
     "should have required property 'OrganisatieCode'"
   )
 })
 
 test_that("test8.json (Invalid JSON) ERROR", {
   expect_error(
-    read_bds(jtf[8], schema = "bds_schema_str.json"),
+    read_bds(jtf[8], schema = schema),
     "lexical error: invalid char in json text."
   )
 })
 
 test_that("test9.json (Bdsnummer 19 missing) MESS", {
   expect_message(
-    read_bds(jtf[9], schema = "bds_schema_str.json"),
+    read_bds(jtf[9], schema = schema),
     "verplicht BDS nummer ontbreekt: 19"
   )
 })
 
 test_that("test10.json (Bdsnummer 20 missing) MESS", {
   expect_message(
-    read_bds(jtf[10], schema = "bds_schema_str.json"),
+    read_bds(jtf[10], schema = schema),
     "verplicht BDS nummer ontbreekt: 20"
   )
 })
 
 test_that("test11.json (Bdsnummer 82 missing) MESS", {
-  expect_message(read_bds(jtf[11], schema = "bds_schema_str.json"),
+  expect_message(read_bds(jtf[11], schema = schema),
     "BDS 82 (Zwangerschapsduur in dagen) heeft geen waarde",
     fixed = TRUE
   )
 })
 
 test_that("test12.json (Bdsnummer 91 missing) PASSES", {
-  expect_silent(read_bds(jtf[12], schema = "bds_schema_str.json"))
+  expect_silent(read_bds(jtf[12], schema = schema))
 })
 
 test_that("test13.json (Bdsnummer 110 missing) MESS", {
-  expect_message(read_bds(jtf[13], schema = "bds_schema_str.json"),
+  expect_message(read_bds(jtf[13], schema = schema),
     "BDS 110 (Geboortegewicht in grammen: heeft geen waarde",
     fixed = TRUE
   )
 })
 
 test_that("test14.json (empty file) ERROR", {
-  expect_error(read_bds(jtf[14], schema = "bds_schema_str.json"), "premature EOF")
+  expect_error(read_bds(jtf[14], schema = schema), "premature EOF")
 })
 
 test_that("test15.json (Bdsnummer 62 numeric) MESS", {
-  expect_message(read_bds(jtf[15], schema = "bds_schema_str.json"))
+  expect_message(read_bds(jtf[15], schema = schema))
 })
 
 test_that("test16.json (Bdsnummer 20 numeric) PASSES", {
-  expect_silent(read_bds(jtf[16], schema = "bds_schema_str.json"))
+  expect_silent(read_bds(jtf[16], schema = schema))
 })
 
 test_that("test17.json (Bdsnummer 82 numeric) PASSES", {
-  expect_silent(read_bds(jtf[17], schema = "bds_schema_str.json"))
+  expect_silent(read_bds(jtf[17], schema = schema))
 })
 
 test_that("test18.json (Bdsnummer 91 numeric) MESS", {
   expect_message(
-    read_bds(jtf[18], schema = "bds_schema_str.json"),
+    read_bds(jtf[18], schema = schema),
     '[{"bdsnummer":91,"description":"Smoking during pregnancy","expected":"one of: 1, 2, 99","supplied":"1","supplied_type":"numeric"}]'
   )
 })
 
 test_that("test19.json (Bdsnummer 110 numeric) PASSES", {
-  expect_silent(read_bds(jtf[19], schema = "bds_schema_str.json"))
+  expect_silent(read_bds(jtf[19], schema = schema))
 })
 
 test_that("test20.json (missing Groepen) MESS", {
-  expect_message(read_bds(jtf[20], schema = "bds_schema_str.json"),
+  expect_message(read_bds(jtf[20], schema = schema),
     "Missing 'ClientGegevens$Groepen'",
     fixed = TRUE
   )
 })
 
 test_that("test21.json (minimal data) MESS", {
-  expect_message(read_bds(jtf[21], schema = "bds_schema_str.json"),
+  expect_message(read_bds(jtf[21], schema = schema),
     "Missing 'Contactmomenten'",
     fixed = TRUE
   )
 })
 
 test_that("test22.json (gad (=49) out of range 50-350: set to NA)", {
-  expect_message(read_bds(jtf[22], schema = "bds_schema_str.json"))
+  expect_message(read_bds(jtf[22], schema = schema))
 })
 
 test_that("test23.json (multiple messages) MESS", {
-  expect_message(read_bds(jtf[23], schema = "bds_schema_str.json"))
+  expect_message(read_bds(jtf[23], schema = schema))
 })
 
 test_that("test24.json (new DDI fields) SILENT", {
-  expect_silent(read_bds(jtf[24], schema = "bds_schema_str.json"))
+  expect_silent(read_bds(jtf[24], schema = schema))
 })
 
 fn <- system.file("extdata", "smocc", "Laura_S.json", package = "jamestest")
 js <- jsonlite::toJSON(jsonlite::fromJSON(fn), auto_unbox = TRUE)
 
 test_that("Laura_S.json is silent with GA in days", {
-  expect_silent(read_bds(js, schema = "bds_schema_str.json"))
+  expect_silent(read_bds(js, schema = schema))
 })
 
 # 2 problematic json files identified by Allegro Sultum - Feb 2020
@@ -162,7 +165,7 @@ fn <- system.file("extdata", "test", "not_a_vector.json", package = "jamestest")
 js <- jsonlite::toJSON(jsonlite::fromJSON(fn), auto_unbox = TRUE)
 
 test_that("not_a_vector.json produces messages", {
-  expect_message(read_bds(js, schema = "bds_schema_str.json"),
+  expect_message(read_bds(js, schema = schema),
     "BDS 82",
     fixed = TRUE
   )
@@ -173,7 +176,7 @@ fn <- system.file("extdata", "test", "http400.json", package = "jamestest")
 js <- jsonlite::toJSON(jsonlite::fromJSON(fn), auto_unbox = TRUE)
 
 test_that("http400.json proceeds silent - no biological mother", {
-  expect_silent(read_bds(js, schema = "bds_schema_str.json"))
+  expect_silent(read_bds(js, schema = schema))
 })
 
 # Check proper splitting of head lag item
--- tests/testthat/test-verify.R
+++ tests/testthat/test-verify.R
@@ -1,3 +1,6 @@
+schema <- "bds_schema_str.json"
+# schema <- "bds_schema.json"
+
 # test the empty object
 js1 <- '{"OrganisatieCode":0,"ClientGegevens":{}}'
 test_that("handles the empty individual object", {
@@ -10,36 +13,36 @@ test_that("handles the empty individual object", {
 jtf <- system.file("extdata", "test", paste0("test", 1:24, ".json"), package = "jamestest")
 
 test_that("test1.json (client3.json) passes convert_individual_bds()", {
-  expect_silent(verify(jtf[1], schema = "bds_schema_str.json"))
+  expect_silent(verify(jtf[1], schema = schema))
 })
 
 test_that("test2.json (missing Referentie) PASSES", {
-  expect_silent(verify(jtf[2], schema = "bds_schema_str.json"))
+  expect_silent(verify(jtf[2], schema = schema))
 })
 
 test_that("test3.json (missing OrganisatieCode) WARNS", {
   expect_message(
-    verify(jtf[3], schema = "bds_schema_str.json"),
+    verify(jtf[3], schema = schema),
     "should have required property 'OrganisatieCode'"
   )
 })
 
 test_that("test4.json (wrong type) WARNS", {
   expect_message(
-    verify(jtf[4], schema = "bds_schema_str.json"),
+    verify(jtf[4], schema = schema),
     ".OrganisatieCode should be integer"
   )
 })
 
 test_that("test5.json (missing ClientGegevens) WARNS", {
   expect_message(
-    verify(jtf[5], schema = "bds_schema_str.json"),
+    verify(jtf[5], schema = schema),
     "should have required property 'ClientGegevens'"
   )
 })
 
 test_that("test6.json (Missing ContactMomenten) WARNS", {
-  expect_message(verify(jtf[6], schema = "bds_schema_str.json"),
+  expect_message(verify(jtf[6], schema = schema),
     "Missing 'Contactmomenten'",
     fixed = TRUE
   )
@@ -47,111 +50,111 @@ test_that("test6.json (Missing ContactMomenten) WARNS", {
 
 test_that("test7.json (Missing Referentie & OrganisatieCode) WARNS", {
   expect_message(
-    verify(jtf[7], schema = "bds_schema_str.json"),
+    verify(jtf[7], schema = schema),
     "should have required property 'OrganisatieCode'"
   )
 })
 
 test_that("test8.json (Invalid JSON) ERROR", {
   expect_error(
-    verify(jtf[8], schema = "bds_schema_str.json"),
+    verify(jtf[8], schema = schema),
     "lexical error: invalid char in json text."
   )
 })
 
 test_that("test9.json (Bdsnummer 19 missing) returns warning", {
   expect_message(
-    verify(jtf[9], schema = "bds_schema_str.json"),
+    verify(jtf[9], schema = schema),
     "verplicht BDS nummer ontbreekt: 19"
   )
 })
 
 test_that("test10.json (Bdsnummer 20 missing) return warning", {
   expect_message(
-    verify(jtf[10], schema = "bds_schema_str.json"),
+    verify(jtf[10], schema = schema),
     "verplicht BDS nummer ontbreekt: 20"
   )
 })
 
 test_that("test11.json (Bdsnummer 82 missing) return message", {
-  expect_message(verify(jtf[11], schema = "bds_schema_str.json"),
+  expect_message(verify(jtf[11], schema = schema),
     "BDS 82 (Zwangerschapsduur in dagen) heeft geen waarde",
     fixed = TRUE
   )
 })
 
 test_that("test12.json (Bdsnummer 91 missing) PASSES", {
-  expect_silent(verify(jtf[12], schema = "bds_schema_str.json"))
+  expect_silent(verify(jtf[12], schema = schema))
 })
 
 test_that("test13.json (Bdsnummer 110 missing) returns message", {
-  expect_message(verify(jtf[13], schema = "bds_schema_str.json"),
+  expect_message(verify(jtf[13], schema = schema),
     "BDS 110 (Geboortegewicht in grammen: heeft geen waarde",
     fixed = TRUE
   )
 })
 
 test_that("test14.json (empty file) returns ERROR", {
-  expect_error(verify(jtf[14], schema = "bds_schema_str.json"), "premature EOF")
+  expect_error(verify(jtf[14], schema = schema), "premature EOF")
 })
 
 test_that("test15.json (Bdsnummer 19 numeric) returns message", {
   expect_message(
-    verify(jtf[15], schema = "bds_schema_str.json"),
+    verify(jtf[15], schema = schema),
     '[{"bdsnummer":19,"description":"Sex of child","expected":"one of: 0, 1, 2, 3","supplied":"2","supplied_type":"numeric"},{"bdsnummer":62,"description":"Caretaker relation","expected":"one of: 01, 02, 03, 04, 05, 06, 07, 08, 98","supplied":"1","supplied_type":"numeric"}]'
   )
 })
 
 test_that("test16.json (Bdsnummer 20 numeric) PASSES", {
-  expect_silent(verify(jtf[16], schema = "bds_schema_str.json"))
+  expect_silent(verify(jtf[16], schema = schema))
 })
 
 test_that("test17.json (Bdsnummer 82 numeric) PASSES", {
-  expect_silent(verify(jtf[17], schema = "bds_schema_str.json"))
+  expect_silent(verify(jtf[17], schema = schema))
 })
 
 test_that("test18.json (Bdsnummer 91 numeric) produces message", {
   expect_message(
-    verify(jtf[18], schema = "bds_schema_str.json"),
+    verify(jtf[18], schema = schema),
     '[{"bdsnummer":91,"description":"Smoking during pregnancy","expected":"one of: 1, 2, 99","supplied":"1","supplied_type":"numeric"}]'
   )
 })
 
 test_that("test19.json (Bdsnummer 110 numeric) PASSES", {
-  expect_silent(verify(jtf[19], schema = "bds_schema_str.json"))
+  expect_silent(verify(jtf[19], schema = schema))
 })
 
 test_that("test20.json (missing Groepen) produces message", {
-  expect_message(verify(jtf[20], schema = "bds_schema_str.json"),
+  expect_message(verify(jtf[20], schema = schema),
     "Missing 'ClientGegevens$Groepen'",
     fixed = TRUE
   )
 })
 
 test_that("test21.json (minimal data) WARNS", {
-  expect_message(verify(jtf[21], schema = "bds_schema_str.json"),
+  expect_message(verify(jtf[21], schema = schema),
     "Missing 'Contactmomenten'",
     fixed = TRUE
   )
 })
 
 test_that("test22.json (range checking) PASSES", {
-  expect_message(verify(jtf[22], schema = "bds_schema_str.json"))
+  expect_message(verify(jtf[22], schema = schema))
 })
 
 test_that("test23.json (multiple messages) produces message", {
-  expect_message(verify(jtf[23], schema = "bds_schema_str.json"))
+  expect_message(verify(jtf[23], schema = schema))
 })
 
 test_that("test24.json (new DDI fields) PASSES", {
-  expect_silent(verify(jtf[24], schema = "bds_schema_str.json"))
+  expect_silent(verify(jtf[24], schema = schema))
 })
 
 fn <- system.file("extdata", "smocc", "Laura_S.json", package = "jamestest")
 js <- jsonlite::toJSON(jsonlite::fromJSON(fn), auto_unbox = TRUE)
 
 test_that("Laura_S.json is silent with GA in days", {
-  expect_silent(verify(js, schema = "bds_schema_str.json"))
+  expect_silent(verify(js, schema = schema))
 })
 
 # 2 problematic json files identified by Allegro Sultum - Feb 2020
@@ -159,7 +162,7 @@ fn <- system.file("extdata", "test", "not_a_vector.json", package = "jamestest")
 js <- jsonlite::toJSON(jsonlite::fromJSON(fn), auto_unbox = TRUE)
 
 test_that("not_a_vector.json produces messages", {
-  expect_message(verify(js, schema = "bds_schema_str.json"))
+  expect_message(verify(js, schema = schema))
 })
 
 # problematic json file http400.json identified by Allegro Sultum - Feb 2020
@@ -167,7 +170,7 @@ fn <- system.file("extdata", "test", "http400.json", package = "jamestest")
 js <- jsonlite::toJSON(jsonlite::fromJSON(fn), auto_unbox = TRUE)
 
 test_that("http400.json proceeds silent - no biological mother", {
-  expect_silent(verify(js, schema = "bds_schema_str.json"))
+  expect_silent(verify(js, schema = schema))
 })
 
 # test battery - comment out to activate
