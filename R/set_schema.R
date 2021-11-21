c893ab70dea364402f0bd04c55fce63b992fd057
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-11-21 14:42:37 +0100

    Set return of set_schema()$schema always to long file name

--- R/set_schema.R
+++ R/set_schema.R
@@ -2,16 +2,15 @@
 #'
 #' @param format String. JSON data schema version number. There are currently
 #'   three schemas supported: `1.0`, `1.1` and `2.0`.
-#' @param schema A file name (including path) with the JSON validation schema.
+#' @param schema A file name (optionally including the path) with the JSON
+#'   validation schema.
 #'   The `schema` argument overrides `format`. The function extracts the
 #'   version number for the basename, and overwrites the
 #'   `format` argument by version number.
-#' @return A list with components `format`, `schema` and `schema_base`.
+#' @return A list with components `format` (either `1.0`, `1.1` or `2.0`),
+#' `schema` (long file name) and `schema_base` (base file name).
 #' @examples
 #' set_schema("2.0")
-#'
-#' # for development
-#' set_schema(schema = "mydir/bds_v3.0.json")
 #' @export
 set_schema <- function(format = "1.0", schema = NULL) {
   if (is.null(schema)) {
@@ -26,6 +25,7 @@ set_schema <- function(format = "1.0", schema = NULL) {
     # extract format by file name extract
     schema_base <- basename(schema)
     format <- gsub("bds_v(.+).json", "\\1", schema_base)
+    schema <- system.file("schemas", schema_base, package = "bdsreader", mustWork = TRUE)
   }
   list(format = format, schema = schema, schema_base = schema_base)
 }
