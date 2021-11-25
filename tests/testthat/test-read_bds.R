635408ed89d3671363c5e0f0704594f5d01e4ab4
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-11-25 11:09:03 +0100

    Add test for issue #4

--- tests/testthat/test-read_bds.R
+++ tests/testthat/test-read_bds.R
@@ -9,3 +9,16 @@ q <- read_bds(data2, format = "2.0")
 test_that("Format 1.0 and 2.0 produce both D-score", {
   expect_equal(table(t$yname), table(q$yname))
 })
+
+fn2 <- system.file("extdata", "bds_v2.0", "smocc", "Laura_S.json", package = "jamesdemodata")
+js2a <- readLines(fn2)
+js2b <- jsonlite::toJSON(jsonlite::fromJSON(fn2), auto_unbox = TRUE)
+
+test_that("Format 2.0 is read after readLines(filename)", {
+  expect_s3_class(read_bds(js2a), "tbl_df")
+})
+
+test_that("Format 2.0 is read after toJSON(fromJSON(filename))", {
+  expect_s3_class(read_bds(js2b), "tbl_df")
+})
+
