ffb47090a814b89cec3f2f83167945911031e9f7
ArjanHuizing <arjanhjhuizing@gmail.com>
2023-04-10 21:32:12 +0200

    parsing errors on new format

--- R/parse_valid.R
+++ R/parse_valid.R
@@ -58,12 +58,37 @@ parse_valid <- function(valid) {
     )
 
     # For misspecified values - return supplied and accepted values
-    val.err <- t(simplify2array(w[w$keyword == "anyOf", "data"]))
+    val.err <- t(simplify2array(w[w$keyword == "anyOf" & !grepl("/clientMeasurements", w$dataPath), "data"]))
+
+
+    # find specific items in clientMeasurements with issues
+    val.err.cm <- t(simplify2array(w[w$keyword == "anyOf" & grepl("/clientMeasurements", w$dataPath), "data"]))
+    if (ncol(val.err.cm) >= 1L) {
+      # outcomes that were deemed incorrect
+      cm.vals <- simplify2array(w[!is.na(w$parentSchema$type)  & grepl("/clientMeasurements", w$dataPath), "data"])
+      cmlist <- list()
+      # check in each bds if it contains problem values
+      for (i in 1L:nrow(val.err.cm)) {
+        vals <- val.err.cm[i, 2L][[1L]][, 2]
+        cmlist[[i]] <- data.frame("bdsnummer" = val.err.cm[i, 1L][[1L]],
+                                  "value" = vals[vals %in% cm.vals])
+      }
+
+      # combine with val.err
+      cmlist <- simplify2array(cmlist)
+      if (nrow(val.err.cm) > 1) cmlist <- t(cmlist)
+      if (nrow(val.err) >= 1) {
+        val.err <- rbind(val.err, cmlist)
+      } else {
+        val.err <- cmlist
+        }
+    }
+
 
     # FIXME
     # creating the warning data frame does not work for JSON string, format 2.0
     # the hack below escape the creation of the supplied entry
-    if (length(val.err[1, 1][[1]] == 3)) {
+    if (length(val.err[1, 1][[1]]) == 3) {
       return(mess)
     }
 
