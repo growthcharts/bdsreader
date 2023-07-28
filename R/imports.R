4cb8ce7f7561e224278e86f1cb5b093d9429492b
Stef van Buuren <stef.vanbuuren@tno.nl>
2023-07-28 13:08:19 +0200

    Remove unused imported functions

--- R/imports.R
+++ R/imports.R
@@ -1,8 +1,7 @@
 #' @importFrom centile         y2z
-#' @importFrom curl            curl
 #' @importFrom dplyr           %>% all_of arrange bind_cols bind_rows
 #'                             filter full_join left_join mutate
-#'                             num_range recode rename right_join select
+#'                             num_range recode rename select
 #' @importFrom dscore          dscore
 #' @importFrom jsonlite        fromJSON prettify toJSON validate
 #' @importFrom jsonvalidate    json_validate
@@ -11,7 +10,7 @@
 #' @importFrom readr           read_lines
 #' @importFrom rlang           .data catch_cnd
 #' @importFrom stats           na.omit
-#' @importFrom tidyr           drop_na pivot_longer pivot_wider tibble
+#' @importFrom tidyr           drop_na pivot_longer pivot_wider
 #' @importFrom tibble          add_column tibble tibble_row
 #' @importFrom utils           hasName
 NULL
